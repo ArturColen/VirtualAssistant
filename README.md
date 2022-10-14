2e3de8bb75bc66374e8dca310805f5a9e60f8f0e
Artur Bomtempo <arturbomtempo9193@gmail.com>
2022-10-14 01:49:03 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Creating a virtual assistant with Python
+Project developed through a video lesson from [Digital Innovation One](https://www.dio.me/) with the goal of introducing knowledge about IoT and Machine Learning. For this, a virtual assistant was created using the Python programming language, along with the OS and Speech Recognition libraries.
+
+# 🔨 Project functionality
+The main function of this project is to make the virtual assistant perform some simple functions, such as typing what the user said, opening Google Chrome and Excel.
+
+# 💻 Technologies and techniques used 
+* `Python:` Activate the device's microphone, understand what the user said, and perform the actions
+
+# 📁 Project access
+You can [access the source code](https://github.com/ArturColen/VirtualAssistant).
---
+++ virtual-assistant.py
@@ -0,0 +1,42 @@
+print('Testando...')
+
+# Import libraries
+import speech_recognition as sr
+import os
+
+# Function to hear and recognize speech
+def listen_microphone():
+    # Enable the user's microphone
+    microphone = sr.Recognizer()
+
+    # Use Microphone
+    with sr.Microphone() as source:
+        # Call an algorithm for noise reduction in sound
+        microphone.adjust_for_ambient_noise(source)
+
+        # Phrase for the user to say something
+        print('Diga alguma coisa: ')
+
+        # Store what the user said in a variable
+        audio = microphone.listen(source)
+
+    try:
+        # Pass the variable to the pattern recognition algorithm
+        phrase = microphone.recognize_google(audio, language='pt-BR')
+
+        if 'navegador' in phrase:
+            os.system('start Chrome.exe')
+
+        elif 'Excel' in phrase:
+            os.system('start Excel.exe')
+
+        # Return the phrase pronounced
+        print('Você disse: ' + phrase)
+
+    # Phrase to be displayed if it does not recognize the speech pattern
+    except sr.UnkownValueError:
+        print('Não entendi!')
+
+    return phrase
+
+listen_microphone()
